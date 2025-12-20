d8e12d3a79e19d4ff166d06d9fbe3aeddbe99451
Jaswant Panchumarti <jaswant.panchumarti@kitware.com>
2025-12-19 19:34:03 -0500

    chore(docs): Fetch WASM bundle from gitlab package registry across all examples

--- examples/js/simple-app/src/main.js
+++ examples/js/simple-app/src/main.js
@@ -2,6 +2,7 @@ import "./style.css";
 import { createNamespace } from "@kitware/vtk-wasm/vtk";
 import { buildWASMScene } from "./example";
 
-createNamespace().then((vtk) => {
+const VTK_WASM_TARBALL_URL = "https://gitlab.kitware.com/api/v4/projects/13/packages/generic/vtk-wasm32-emscripten/9.5.20251215/vtk-9.5.20251215-wasm32-emscripten.tar.gz";
+createNamespace(VTK_WASM_TARBALL_URL).then((vtk) => {
   buildWASMScene(vtk, "#app > canvas");
 });
